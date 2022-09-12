7369f03ddcc6952562912ccc7172e4fb50579173
Honza S. Copak <bonum.comunae@volny.cz>
2022-09-12 11:42:50 +0200

    add udi-monitor beta

---
+++ mini_terminal.py
@@ -0,0 +1,100 @@
+# This file is part of the octopusLAB project
+# The MIT License (MIT)
+# Copyright (c) 2016-2021 Jan Copak, Petr Kracik, Vasek Chalupnicek
+
+# from mini_terminal import terminal_info...
+import sys
+
+
+SEPARATOR_WIDTH = 39
+
+# --- VT 100 ---
+def get_cursor_position():
+    height=0
+    width=0
+    num=''
+
+    sys.stdout.write('\x1b[6n')
+    while True:
+        char = sys.stdin.read(1)
+        if char is 'R':
+            width = int(num)
+            break
+        if char is '\x1b':
+            width=0
+            height=0
+            num=''
+            continue
+        if char is '[':
+            continue
+        if char is ';':
+            height=int(num)
+            num=''
+            continue
+        num+=char
+    return (width, height)
+
+
+def terminal_info():
+    # Save cursor position
+    sys.stdout.write('\x1b[s')
+    
+    #Move cursor outside of screen
+    sys.stdout.write('\x1b[999B\x1b[999C')
+    (w, h) = get_cursor_position()
+    
+    # Restore cursor position
+    sys.stdout.write('\x1b[u')
+
+    print ("terminal_size: ", w, h)
+ 
+ 
+def terminal_clear():
+    # def clear():
+    print(chr(27) + "[2J")  # clear terminal
+    print("\x1b[2J\x1b[H")  # cursor up
+
+
+def terminal_color(txt,col=33): # default yellow
+    # 30 black / 31 red / 32 green / 33 yellow / 34 blue / 35 violet / 36 cyan 
+    # 21 underline
+    # print("\033[32mgreen\033[m")
+    return "\033[" + str(col) + "m" + str(txt) + "\033[m"
+
+
+def runningEffect(num = 16):
+    from time import sleep_ms
+    for ii in range(num):
+        print(".",end="")
+        sleep_ms(20)
+    print()
+
+
+def printBar(num1,num2,char="|",col1=32,col2=33):
+    print("[",end="")
+    print((("\033[" + str(col1) + "m" + str(char) + "\033[m")*num1),end="")
+    print((("\033[" + str(col2) + "m" + str(char) + "\033[m")*num2),end="")
+    print("]  ",end="")
+
+
+# ---------------
+
+def printHead(s):
+    print()
+    print('-' * SEPARATOR_WIDTH)
+    print("[--- " + s + " ---] ")
+
+
+def printTitle(t,w=SEPARATOR_WIDTH):
+    print()
+    print('=' * w)
+    print("|",end="")
+    print(t.center(w-2),end="")
+    print("|")
+    print('=' * w)
+
+
+def printLog(i,s=""):
+    print()
+    print('-' * SEPARATOR_WIDTH)
+    print("[--- " + str(i) + " ---] " + s)
---
+++ udim.py
@@ -0,0 +1,131 @@
+"""
+Universal Digital Interface - Monitor
+(c) 2016-22 OctopusLab
+
+"""
+
+from time import sleep, sleep_ms
+from utils.bits import neg, reverse, get_bit, set_bit # int2bin
+from universal_digital_interface import Universal_interface
+from universal_digital_interface import bin_str2int, int2bin_str8, num_to_bytes, num_to_hex_str4, num_to_hex_str2
+from mini_terminal import terminal_info, terminal_color, terminal_clear
+
+ui = Universal_interface()
+
+vROM = [] # virual ROM
+
+
+# -------------------------------------
+DISPLAY7 = True
+    
+if DISPLAY7:
+    from utils.octopus import disp7_init
+    d7 = disp7_init()
+
+def print_help():
+    print("-"*39)
+    print(" Universal Digital Interface - Monitor")
+    print(" HELP")
+    print("-"*39)
+    print("Copy         C <start> <end> <dest>")
+    print("Dump         D <start>")
+    print("Go           G <address>")
+    print("Help         H")
+    print("Clear screen L")
+    print("Info         I")
+    print("Options      O")
+    print("Read         R <address>")
+    print("Write        W <address> <data>")
+    
+    print("-"*39)
+    
+ 
+# =======================================================
+while True:
+    terminal_clear()
+    print("UDI Monitor 0.2 OctopusLAB 2016-22")
+    
+    try:
+       input_str = input(terminal_color("> ", 32))
+    except KeyboardInterrupt:
+        print('^C')
+        continue
+    except EOFError:
+        print()
+                
+    try:
+        cmd = input_str.split()
+        cmd0 = cmd[0]
+    except EOFError:
+        print("debug: ERR split")
+        
+    if cmd0.upper() == "H": print_help()
+    if cmd0.upper() == "L": terminal_clear()
+    if cmd0.upper() == "T": terminal_info() # fix it
+    
+    if cmd0.upper() == "R":
+        addr = int(cmd[1])
+        
+        bytes2 = num_to_bytes(addr)
+        ui.i2c.writeto(39, bytes2)
+        # data =  num_to_hex_str4(addr)
+        data8 = num_to_hex_str2(ui.read16d()[1])
+        print("read", num_to_hex_str4(addr), data8)
+        
+
+    if cmd0.upper() == "D":
+        addr = int(cmd[1])
+        
+        for r in range(8):
+            print()
+            print(num_to_hex_str4(addr+r*16), end="")
+        
+            for i in range(16):
+                bytes2 = num_to_bytes(addr+r*16+i)
+                ui.i2c.writeto(39, bytes2)
+                data8 = num_to_hex_str2(ui.read16d()[1])
+                print(" ", data8, end="")
+            
+            
+                
+"""
+#ui.write16(b'\xFF\xFF')
+#ue.write16(num_to_bytes(0b1111111111111111))
+#exp16.write(255)
+bytes2 = num_to_bytes(i)
+ui.i2c.writeto(39, bytes2)
+print(loop2, i, num_to_hex_str4(i))
+
+
+for loop3 in range(32):
+    i = randint(0,65536)
+    bytes2 = num_to_bytes(i)
+    ui.i2c.writeto(39, bytes2)
+    print(loop3, num_to_hex_str4(i), i)
+    sleep_ms(200)
+
+
+print("--- test 256 --- loop")
+for i in range(2**8): # 256
+    bytes2 = num_to_bytes(i)
+    ui.write16(bytes2)    
+    bin_str = int2bin_str8(i)
+    print(num_to_hex_str2(i), int2bin_str8(i), int2bin_str8(reverse(i)), bin_str2int(bin_str)) # num_to_bytes(i,rev=False))
+    sleep_ms(10)
+
+
+sleep(2)
+print("--- test 64k --- and read data8")
+for i in range(2**16): # 65536
+    bytes2 = num_to_bytes(i)
+    ui.write16(bytes2)
+    data8 = num_to_hex_str2(ui.read16d()[1])
+    
+    # max speed / "REM"
+    # num_to_hex_str(i) # hex(int("0xaa"))
+    print(i, num_to_hex_str4(i), data8, num_to_bytes(i,rev=False))
+    # sleep_ms(1000)
+    if DISPLAY7:
+        d7.show(num_to_hex_str4(i)+"  "+data8)
+    sleep(0.3)
+"""
