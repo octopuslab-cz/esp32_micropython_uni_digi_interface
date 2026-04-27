48f74ccc72a5ae1573ef11c9fa5e1a405a75d1d7
Honza S. Copak <bonum.comunae@volny.cz>
2026-04-27 18:38:05 +0200

    add WS byte

--- components/udi_hw.py
+++ components/udi_hw.py
@@ -1,4 +1,8 @@
 from micropython import const
+from machine import Pin, I2C
+from components.i2c_expander import Expander16
+from utils.pinout import set_pinout
+pinout = set_pinout()
 
 
 def init_led(led_pin=2):
@@ -7,12 +11,41 @@ def init_led(led_pin=2):
     return led
 
 
+def init_rgb(pin,num=1):
+    from components.ws_rgb import Rgb
+    ws = Rgb(10,num)
+    # ws = Rgb(pinout.PWM2_PIN,16)
+    return ws
+
+
+def rgb_fill(ws, offset=0,c=(0,0,0)):
+    for i in range(8):
+        ws.color(c,i+offset)
+
+
+def show_byte(ws, value, offset=8, reverse=True, color_on=(0,50,0), color_off=(0,0,0)):
+    for i in range(8):
+        bit = (value >> i) & 1  # i-bit (LSB = i=0)
+
+        if reverse:
+            idx = (7 - i) + offset
+        else:
+            idx = i + offset
+
+        if bit:
+            ws.color(color_on, idx)
+        else:
+            ws.color(color_off, idx)
+
+
 def i2c_init(HW_or_SW=0,freq=100000):
     print("- i2c init")
-    from machine import Pin, I2C
-    I2C_SDA_PIN, I2C_SCL_PIN = const(21), const(22)
+    # I2C_SDA_PIN, I2C_SCL_PIN = const(21), const(22)
+    print(pinout.I2C_SCL_PIN,pinout.I2C_SDA_PIN)
+    i2c = I2C(scl=Pin(HW_or_SW, pinout.I2C_SCL_PIN), sda=Pin(pinout.I2C_SDA_PIN), freq=freq)
+
     # HW_or_SW: HW 0 | SW 1
-    i2c = I2C(HW_or_SW, scl=Pin(I2C_SCL_PIN), sda=Pin(I2C_SDA_PIN), freq=freq)
+    ## i2c = I2C(HW_or_SW, scl=Pin(I2C_SCL_PIN), sda=Pin(I2C_SDA_PIN), freq=freq)
     return i2c
 
 
@@ -21,7 +54,7 @@ def lcd4_show(lcd,s="",r=0,c=0):
     lcd.putstr(s)
 
 
-def lcd4_init(i2c):
+def lcd4_init():
     print("- lcd4_init")
     from lib.esp8266_i2c_lcd import I2cLcd
     lcd = I2cLcd(i2c, 63, 4, 16) # addr, rows, col
@@ -31,12 +64,10 @@ def lcd4_init(i2c):
     return lcd
 
 
-def exp16_init():
-    from components.led import Led
-    from components.i2c_expander import Expander16
- 
-    clk = Led(26)
+def exp16_init(i2c):
+     
+    #clk = Led(26)
     b2 = bytearray(16 // 8) # temp 2 bytes    
-    e16 = Expander16(39) # addr default 000 > 0x20
+    e16 = Expander16(39, i2c) # addr default 000 > 0x20
     
-    return e16, b2, clk
+    return e16, b2
