48ac9e2a45b256c1d71886beb2c27561f67f424b
Honza S. Copak <bonum.comunae@volny.cz>
2026-05-02 14:17:05 +0200

    Add files via upload

--- examples/example06_s80.asm
+++ examples/example06_s80.asm
@@ -1,24 +1,27 @@
-; example06 - simple data_string
-     #DATA = "octopus test"
-; fix virtual RAM addr: 256+ (0x01,0x00+)
-; org 0x00
-;------------------------
+; example06_s80.asm
+; Copy 16 bytes from 0x0050 → 0x0030
+; HL = source, DE = destination, C = counter
+
+    JMP start
+
 start:
-    MVI_L 0x00 ; 
-    MVI_H 0x01
-    MOV_A,M
-    INR_L
-    MOV_A,M
-    INR_L
-    MOV_A,M
-;
-    LDA 0x01 0x00 ; note
-    LDA 0x01 0x01
-    LDA 0x01 0x02
-    NOP
-    MVI_A 45 ; ord("-")=45
-    STA 0x01 0x0C
-; 
-    NOP
-end.
-;
+    #DATA = "octopus test  "  ; → mem[0x0050..0x005F], 16 chars
+
+    LXI_H 0x0050       ; HL = source address
+    LXI_D 0x0030       ; DE = destination address
+    MVI_C 0x10         ; C  = 16 (byte counter)
+
+copy_loop:
+    MOV_A,M            ; A = mem[HL]
+    STAX_D             ; mem[DE] = A
+    INX_H              ; HL++
+    INX_D              ; DE++
+    DCR_C              ; C--
+    JNZ copy_loop      ; repeat while C != 0
+
+    ; verify: read first copied byte back
+    LXI_H 0x0030       ; HL = destination
+    MOV_A,M            ; A = mem[0x0030]  should be 'o' = 0x6F
+    MOV_A,A            ; [debug hook] print A/B/C
+    HLT
+ end.
